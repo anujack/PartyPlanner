9dfab4a1ee77befbfb391935d18e5b996b8b4bfc
anujack <44030765+anujack@users.noreply.github.com>
2018-10-10 13:29:41 -0400

    Add files via upload
    Added Plan.html and planner.php

--- PartyPlanner/index.php
+++ PartyPlanner/index.php
@@ -2,10 +2,14 @@
 session_start();
 
 if(isset($_SESSION['email'])){
-    echo "<form action='forms/logout.php' method='POST'>
+    echo "<a href='Login.html'>Login Form</a><br>
+    <a href='Signup.html'>SignUp Form</a><br>
+    <a href='Plan.html'>Planner</a><br>
+    <form action='forms/logout.php' method='POST'>
     <button type='submit' name='submit' class='btn btn-default'>Logout</button>
     </form>";
 } else {
     echo "<a href='Login.html'>Login Form</a><br>
-    <a href='Signup.html'>SignUp Form</a><br>";
+    <a href='Signup.html'>SignUp Form</a><br>
+    <a href='Plan.html'>Planner</a>";
 }
